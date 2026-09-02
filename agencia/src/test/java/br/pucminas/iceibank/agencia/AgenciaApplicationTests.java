e43d2ef811d6c3ffa1fdbf4df225f88850405ef3
Rodrigo Franchini Cecchin <rofranchinicecchin@gmail.com>
2026-09-02 12:06:09 -0300

    chore: substitui esqueleto Node por projeto Spring Boot (Maven, Java 21)

---
+++ agencia/src/test/java/br/pucminas/iceibank/agencia/AgenciaApplicationTests.java
@@ -0,0 +1,12 @@
+package br.pucminas.iceibank.agencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenciaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
